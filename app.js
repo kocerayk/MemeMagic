f0a1fd74b4a021e223bd1e02c4bd308e4820fc85
eraykkoc <123729330+eraykkoc@users.noreply.github.com>
2024-09-25 03:48:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const generateMemeBtn = document.getElementById("generate-meme-btn");
+const memeImage = document.querySelector(".meme-image");
+const memeTitle = document.querySelector(".meme-title");
+const memeAuthor = document.querySelector(".meme-author");
+
+const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author}`;
+};
+
+const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/ProgrammerHumor")
+        .then(response => response.json())
+        .then(data => {
+            updateDetails(data.url, data.title, data.author);
+        });
+};
+
+generateMemeBtn.addEventListener("click", generateMeme);
+generateMeme();
